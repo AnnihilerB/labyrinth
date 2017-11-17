6382ed5460ffdb0ea9911e47ba6203edfc755c25
mduverneix <mduverneix@carioca.emi.u-bordeaux.fr>
2017-11-17 09:58:03 +0100

    Création d'un bouton qui fait afficher un message associé à la touche appuyée

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,44 @@
 package controller;
 
+import model.Model;
+import view.View;
+
+import javafx.event.*;
+import javafx.scene.control.Button;
+import javafx.scene.input.*;
+import javafx.stage.Stage;
+
 public class Controller {
+	private static Controller instance=new Controller();
+	private static View view;
+	private static Model model;
+	
+	public static EventHandler eventHandlerButton = new EventHandler<KeyEvent>() {
+		public void handle(KeyEvent event) {
+				if(event.getCode().equals(KeyCode.UP))
+					System.out.println("test haut");
+				else if(event.getCode()==KeyCode.DOWN)
+					System.out.println("test bas");
+				else if(event.getCode()==KeyCode.LEFT)
+					System.out.println("test gauche");
+				else if(event.getCode()==KeyCode.RIGHT)
+					System.out.println("test droit");
+		}	
+	};
+	
+	
+	private Controller() {
+		view = View.getInstance();
+		model = Model.getInstance();
+	}
+	
+	public static Controller getInstance() {
+		return instance;
+	}
+	
+	public void start(Stage primaryStage) throws Exception {
+		view.start(primaryStage);
+		view.setOnAction();
+	}
+		
 }
