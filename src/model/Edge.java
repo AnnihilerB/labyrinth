af4324006c823be5c6e8eacb3e836cc75f4bc423
Ali <acherifi@etu.u-bordeaux.fr>
2017-11-21 15:39:19 +0100

    Ajout du model Vertex/Edge

---
+++ src/model/Edge.java
@@ -0,0 +1,51 @@
+package model;
+
+import org.jgrapht.graph.DefaultEdge;
+
+public class Edge extends DefaultEdge implements Comparable<Edge> {
+    public enum Type {
+        OPENED_DOOR,
+        CLOSED_DOOR,
+        CORRIDOR
+        }
+
+
+    private Type type;
+
+    public Edge(Type type) {
+        super();
+        this.type = type;
+    }
+
+    //default
+    public Edge() {
+        super();
+        this.type = Type.CORRIDOR;
+    }
+
+    public Vertex getSource() {
+        return (Vertex) super.getSource();
+    }
+
+    public Vertex getTarget() {
+        return (Vertex) super.getTarget();
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        int source = this.getSource().compareTo((o).getSource());
+        if (source != 0)
+            return source;
+        else {
+            return this.getTarget().compareTo((o).getTarget());
+        }
+    }
+}
---
+++ src/model/Vertex.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Vertex {
+
+
+    int compareTo(Vertex v){
+
+        return 0;
+
+    }
+}
