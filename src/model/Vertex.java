062204cf306b3c5be1525e0ab274cd5278c9d0f0
Teiki Pepin <tepepin@emi.u-bordeaux1.fr>
2017-11-24 08:59:37 +0100

    Vertex MaJ

--- src/model/Vertex.java
+++ src/model/Vertex.java
@@ -1,11 +1,58 @@
 package model;
 
+import java.util.Objects;
+
 public class Vertex {
-
-
-    int compareTo(Vertex v){
-
-        return 0;
-
-    }
+	private int x;
+	private int y;
+	
+	
+	public Vertex() {
+		this(0, 0);
+	}
+	
+	public Vertex(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	//Sources: 
+	//https://docs.oracle.com/javase/8/docs/api/java/util/Objects.html
+	//https://docs.oracle.com/javase/8/docs/api/java/lang/Object.html
+	
+	@Override
+	public String toString() {
+		return "(" + x + "," + y +")";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Vertex)
+			if (this.x == ((Vertex) obj).getX() && this.y == ((Vertex) obj).getY())
+				return true;
+		
+		return false;		
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
 }
