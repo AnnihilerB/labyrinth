03584c6075cacb4f617d2373bd88d01c263b0f6f
Ali <cherifiali@outlook.fr>
2017-11-24 10:03:18 +0100

    Création de la classe GlobalView créant

---
+++ src/view/GlobalView.java
@@ -0,0 +1,38 @@
+package view;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import model.Labyrinth;
+
+
+public class GlobalView {
+
+    private static GlobalView globalView = null;
+
+    static Scene scene;
+    static Pane pane = new Pane();
+
+    private GlobalView() {
+
+
+    }
+
+    public static GlobalView getInstance() {
+        if (globalView == null)
+            globalView = new GlobalView();
+        return globalView;
+    }
+
+    public void createGlobalView(Stage stage){
+        LabyrinthView.drawFrame(stage, scene, pane, 16,16);
+        LabyrinthView.drawWall(pane, 1, 1, 1, 2, LabyrinthView.WALL_COLOR);
+
+    }
+}
+
+
+
