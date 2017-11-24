25e7a731b1e6deda1a63997f94f17c5440fc6488
Teiki Pepin <tepepin@emi.u-bordeaux1.fr>
2017-11-24 09:08:38 +0100

    Génération de labyrinth basique, MaJ de Edge temporaire, Main modifié pour tests

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,17 @@ import controller.Controller;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.Labyrinth;
 
 public class Main extends Application {
 
 	static Controller controller;
 
 	public static void main(String args[]) {
-		launch();
+		Labyrinth labyrinth = Labyrinth.getInstance();
+		labyrinth.printLabyrinth();
+		
+		launch();		
 	}
 
 	@Override
