f6bd476cc96cb46123bd4577cfc216330fd40f3a
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2017-12-01 10:00:38 +0100

    Update Character.java

--- src/model/Character.java
+++ src/model/Character.java
@@ -2,13 +2,27 @@ package model;
 
 public abstract class Character extends Element {
 	
+
 	
 	public Character(int _x, int _y) {
 		super(_x,_y);
 	}
 	
-	public void seDeplacer(int _x, int _y) {
-		this.setX(_x);
-		this.setY(_y);
+	public boolean collisionMur(int nx, int ny) {
+		if(Labyrinth.getInstance().areVerticesConnected(this.getX(), this.getY(), nx, ny)) {
+			return true;
+		}
+		else
+			return false;
 	}
+	
+	public void seDeplacer(int _x, int _y) {
+		if(!collisionMur(_x, _y)) {
+			this.setX(_x);
+			this.setY(_y);
+		}
+	}
+
+	
+	
 }
