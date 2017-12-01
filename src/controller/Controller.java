cf8f6382546d8c349a957408ffe0600312e64f2a
Ali <cherifiali@outlook.fr>
2017-12-01 09:12:31 +0100

    Controller refait entièrement pour coller au MVC

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,6 +1,5 @@
 package controller;
 
-import javafx.scene.paint.Color;
 import model.Labyrinth;
 import model.Model;
 
@@ -8,49 +7,52 @@ import javafx.event.*;
 import javafx.scene.input.*;
 import javafx.stage.Stage;
 import view.GlobalView;
+import view.LabyrinthView;
 
-public class Controller implements EventHandler<KeyEvent>{
+
+public class Controller {
 	private static Controller instance = null;
 	private static GlobalView view;
 	private static Model model;
 
-	
-	
-	private Controller() {
-	    view = GlobalView.getInstance();
-		model = Model.getInstance();
-	}
-	
-	public static Controller makeInstance() {
-		if (instance == null)
-		    instance = new Controller();
-		return instance;
-	}
-	
-	public static void start(Stage primaryStage) throws Exception {		
-        GlobalView.getInstance().createGlobalView(primaryStage, Labyrinth.getInstance().getWalls());
-        primaryStage.show();
-	}
+	private static EventHandler<KeyEvent> keyboardListener = new EventHandler<KeyEvent>() {
+		@Override
+		public void handle(KeyEvent event) {
+			switch (event.getCode()) {
 
+				case UP:
+					System.out.println("UP");
+					break;
+				case DOWN:
+					System.out.println("DOWN");
+					break;
+				case LEFT:
+					System.out.println("LEFT");
+					break;
+				case RIGHT:
+					System.out.println("RIGHT");
+					break;
 
-	@Override
-	public void handle(KeyEvent event) {
-		switch(event.getCode()){
+			}
+		}
+	};
 
-			case UP:
-				System.out.println("UP");
-				break;
-			case DOWN:
-				System.out.println("DOWN");
-				break;
-			case LEFT:
-				System.out.println("LEFT");
-				break;
-			case RIGHT:
-				System.out.println("RIGHT");
-				break;
-
+		private Controller() {
+			view = GlobalView.getInstance();
+			model = Model.getInstance();
 		}
 
-	}
+		public static Controller makeInstance() {
+			if (instance == null)
+				instance = new Controller();
+			return instance;
+		}
+
+		public static void start(Stage primaryStage) throws Exception {
+			GlobalView.getInstance().createGlobalView(primaryStage, Labyrinth.getInstance().getWalls());
+			LabyrinthView.getInstance().setOnAction(keyboardListener);
+			primaryStage.show();
+		}
+
+
 }
