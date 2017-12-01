0e0f7e8979ae4535f1d611206428db40424e517c
mduverneix <mduverneix@pelican.emi.u-bordeaux.fr>
2017-12-01 09:59:21 +0100

    Gestion de la collision

--- src/model/Character.java
+++ src/model/Character.java
@@ -9,10 +9,21 @@ public abstract class Character extends Element {
 		super(_x,_y);
 	}
 	
+	public boolean collisionMur(int nx, int ny) {
+		if(Labyrinth.getInstance().areVerticesConnected(this.getX(), this.getY(), nx, ny)) {
+			return true;
+		}
+		else
+			return false;
+	}
+	
 	public void seDeplacer(int _x, int _y) {
-		this.setX(_x);
-		this.setY(_y);
+		if(!collisionMur(_x, _y)) {
+			this.setX(_x);
+			this.setY(_y);
+		}
 	}
 
+	
 	
 }
