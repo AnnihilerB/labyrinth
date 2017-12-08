8160fd7b1a2edf09b0c6a5be1df5895afd46504f
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2017-12-08 10:34:47 +0100

    bug gitHub

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,49 +1,76 @@
 package controller;
 
-import javafx.scene.paint.Color;
 import model.Labyrinth;
 import model.Model;
 
 import javafx.event.*;
 import javafx.scene.input.*;
 import javafx.stage.Stage;
+import model.Vertex;
 import view.GlobalView;
-import view.View;
+import view.LabyrinthView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 
 public class Controller {
 	private static Controller instance = null;
-	private static Model model;
 	private static GlobalView view;
+	private static Model model;
 
-	
-	public static EventHandler eventHandlerButton = new EventHandler<KeyEvent>() {
-		public void handle(KeyEvent event) {
-				if(event.getCode().equals(KeyCode.UP))
-					System.out.println("test haut");
-				else if(event.getCode()==KeyCode.DOWN)
-					System.out.println("test bas");
-				else if(event.getCode()==KeyCode.LEFT)
-					System.out.println("test gauche");
-				else if(event.getCode()==KeyCode.RIGHT)
-					System.out.println("test droit");
-		}	
-	};
-	
-	
-	private Controller() {
-		model = Model.getInstance();
-	    view = GlobalView.getInstance();
-	}
-	
-	public static Controller makeInstance() {
-		if (instance == null)
-		    instance = new Controller();
-		return instance;
-	}
-	
-	public static void start(Stage primaryStage) throws Exception {		
-        GlobalView.getInstance().createGlobalView(primaryStage, Labyrinth.getInstance().getWalls());
-        primaryStage.show();
-	}
-		
+	private static EventHandler<KeyEvent> keyboardListener = event -> {
+        switch (event.getCode()) {
+
+            case UP:
+                System.out.println("UP");
+                break;
+            case DOWN:
+                System.out.println("DOWN");
+                break;
+            case LEFT:
+                System.out.println("LEFT");
+                break;
+            case RIGHT:
+                System.out.println("RIGHT");
+                break;
+
+        }
+    };
+
+		private Controller() {
+			view = GlobalView.getInstance();
+			model = Model.getInstance();
+		}
+
+		public static Controller makeInstance() {
+			if (instance == null)
+				instance = new Controller();
+			return instance;
+		}
+
+		public static void start(Stage primaryStage) throws Exception {
+			ArrayList wallCoordiantes = generateWalls(Labyrinth.getInstance().getWalls());
+			GlobalView.getInstance().createGlobalView(primaryStage, wallCoordiantes);
+			LabyrinthView.getInstance().setOnAction(keyboardListener);
+			primaryStage.show();
+		}
+
+		private static ArrayList generateWalls(Set<List<Vertex>> walls){
+			ArrayList<Integer> wallsCoordinates = new ArrayList();
+			walls.forEach((wall) -> {
+				int xs = wall.get(0).getX();
+				int ys = wall.get(0).getY();
+				int xt = wall.get(1).getX();
+				int yt = wall.get(1).getY();
+				wallsCoordinates.add(xs);
+				wallsCoordinates.add(ys);
+				wallsCoordinates.add(xt);
+				wallsCoordinates.add(yt);
+			});
+			return wallsCoordinates;
+		}
+
+
 }
