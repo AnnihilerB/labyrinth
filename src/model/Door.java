3c6a6575811427d2cf9fa68097d9583ff58c0914
mduverneix <mduverneix@blader.emi.u-bordeaux.fr>
2017-12-08 10:13:07 +0100

    Création de la classe porte

---
+++ src/model/Door.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Door extends ElementInteractif{
+	
+	private Button linkedButton; // bouton lié à la porte
+	
+	public Door(int _x, int _y, Button _button) {
+		super(_x,_y);
+		linkedButton=_button;
+	}
+	
+	public Door(int _x, int _y, Button _button, boolean _state) {
+		super(_x, _y, _state);
+		linkedButton=_button;
+	}
+}
