5f7f97b8adb6b305b564d79c5de3dff9aca3aafa
MartialDuv <33746332+MartialDuv@users.noreply.github.com>
2017-12-08 10:26:21 +0100

    Update ElementView.java
    Bug GitHub

--- src/view/ElementView.java
+++ src/view/ElementView.java
@@ -1,4 +1,42 @@
 package view;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import java.io.File;
+
 public class ElementView {
+    //private final Image PlayerImage = new Image(getClass().getResource("assets/player.png").toExternalForm());
+    private static ElementView elementView = new ElementView();
+    private ImageView playerView;
+
+    private ElementView() {
+    }
+
+    public static ElementView getInstance() {
+        return elementView;
+    }
+
+    public void drawSprite(Pane pane, int nbrX, int nbrY) {
+        File file = new File("assets/player.png");
+        Image image = new Image(file.toURI().toString());
+    	playerView = new ImageView(image);
+
+    	
+    	pane.getChildren().add(this.playerView);
+    	
+    	double xt = (LabyrinthView.WALL + nbrX * (LabyrinthView.WALL + LabyrinthView.CELL)) * LabyrinthView.SPAN;
+    	double yt = (LabyrinthView.WALL + nbrY * (LabyrinthView.WALL + LabyrinthView.CELL)) * LabyrinthView.SPAN;
+    	
+    	playerView.setX(xt);
+    	playerView.setY(yt);
+    }
+    
+    public void updatePlayer(int x, int y) {
+    	playerView.setX(x);
+    	playerView.setY(y); 
+    }
 }
+
+
