8a343aa71c15a1d1dfc02ea044a625c1344aa667
mduverneix <mduverneix@blackmore.emi.u-bordeaux.fr>
2017-12-11 17:14:02 +0100

    Début des commentaires pour la javadoc et changement de nom d'attribut pour Candy

--- src/model/Candy.java
+++ src/model/Candy.java
@@ -1,25 +1,47 @@
 package model;
-
+/**
+ * 
+ * @author mduverneix
+ *
+ */
 public class Candy extends Element {
+	/**
+	 * the number of points we want to add to the score.
+	 */
+	private int numPoint; // le nombre de points qu'on va ajouter au score
 	
-	private int ajoutPoint; // le nombre de points qu'on va ajouter au score
-	
+	/**
+	 * Allocates a new Candy object with 500 number of points and a chosen position.
+	 * @param _x the x coordinate we want for the candy.
+	 * @param _y the y coordinate we want for the candy.
+	 */
 	public Candy(int _x, int _y) { // constructeur par défaut avec un nombre de points à ajouter fixé à 500
 		super(_x,_y);
-		ajoutPoint=500; // cette variable peut être modifiée pour être en accord avec le nombre de points qu'on veut ajouter
+		numPoint=500; // cette variable peut être modifiée pour être en accord avec le nombre de points qu'on veut ajouter
 	}
-	
+	/**
+	 * Allocates a new Candy object with a chosen position and a chosen number of points.
+	 * @param _x the x coordinate we want for the candy.
+	 * @param _y the y coordinate we want for the candy.
+	 * @param _point the number of points we want the candy to give.
+	 */
 	public Candy(int _x, int _y, int _point) { // constructeur pour définir le nombre de points qu'on veut ajouter
 		super(_x, _y);
-		ajoutPoint=_point;
+		numPoint=_point;
 	}
-
-	public int getAjoutPoint() {
-		return ajoutPoint;
+	/**
+	 * Gets the num
+	 * @return
+	 */
+	public int getNumPoint() {
+		return numPoint;
 	}
-
-	public void setAjoutPoint(int ajoutPoint) {
-		this.ajoutPoint = ajoutPoint;
+	/**
+	 * 
+	 * @param ajoutPoint
+	 */
+	public void setNumPoint(int numPoint) {
+		this.numPoint = numPoint;
 	}
 	
 }
