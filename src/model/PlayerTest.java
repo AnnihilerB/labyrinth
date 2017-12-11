e1d1294aff36603a84f349ca8a434caf74d32b56
mduverneix <mduverneix@blackmore.emi.u-bordeaux.fr>
2017-12-11 16:44:39 +0100

    Création de la classe PlayerTest pour les tests de la classe Player

---
+++ src/model/PlayerTest.java
@@ -0,0 +1,28 @@
+package model;
+
+import junit.framework.TestCase;
+
+public class PlayerTest extends TestCase {
+
+	@org.junit.Test
+    public void testPlayer() {
+        Player player=Player.getInstance();
+        assertEquals(0,player.getX());
+        assertEquals(0,player.getY());
+        player.setX(10);
+        player.setY(11);
+        assertEquals(10,player.getX());
+        assertEquals(11,player.getY());
+    }
+	
+	@org.junit.Test
+    public void testCollisionMur() {
+        Player player=Player.getInstance();
+        Labyrinth labyrinth=Labyrinth.getInstance();
+        /*assertEquals(false,labyrinth.areVerticesConnected(player.getX(), player.getY(), player.getX()+1, player.getY()));
+        assertEquals(false,labyrinth.areVerticesConnected(player.getX(), player.getY(), player.getX()-1, player.getY()));
+        assertEquals(false,labyrinth.areVerticesConnected(player.getX(), player.getY(), player.getX(), player.getY()+1));
+        assertEquals(false,labyrinth.areVerticesConnected(player.getX(), player.getY(), player.getX(), player.getY()-1));*/
+    }
+	
+}
