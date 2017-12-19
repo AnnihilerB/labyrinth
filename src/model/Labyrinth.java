c178c79248f3d56c2f755d7316d9f81e13e099b6
pierrinator <plorson3@gmail.com>
2017-12-19 12:57:27 +0100

    Javadoc écrite a 90%

--- src/model/Labyrinth.java
+++ src/model/Labyrinth.java
@@ -14,6 +14,14 @@ import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 
+/**
+ * Create the graph using SimpleGraph. Also contains all the required
+ * methods to check what contains the labyrinth as well as the Manhattan algorithm.
+ *  *
+ * @author 			Teiki Pepin, Pierre Lorson
+ */
+
+
 public class Labyrinth {
 
 	public enum Directions {
@@ -32,6 +40,11 @@ public class Labyrinth {
 
 	private SimpleGraph<Vertex, Edge> labyrinth;
 
+
+	/**
+	 * Create a Labyrinth. Warning : as we're using the Singleton design pattern,
+	 * this method should not be called directly. Use getInstance instead.
+	 */
 	//Constructeur privé (singleton design pattern)
 	private Labyrinth() {
 		labyrinth = new SimpleGraph(Edge.class);
@@ -101,6 +114,12 @@ public class Labyrinth {
 		}
 	}
 
+	/**
+	 * Get a vertex from the vertices set
+	 * @param x Horizontal axis
+	 * @param y Vertical axis
+	 * @return The desired vertex.
+	 */
     public model.Vertex getVertex(int x, int y) {
         Set<Vertex> setVertex = labyrinth.vertexSet();
         for (Vertex vertex : setVertex) {
@@ -110,6 +129,13 @@ public class Labyrinth {
         return null;
     }
 
+	/**
+	 * Calculate the Manhattan distance using the Manhattan algorithm
+	 * between two vertices.
+	 * Should not be called. Use launchManhattan instead.
+	 * @param source Starting vertex.
+	 * @param target Goal vertex.
+	 */
     private void calculateManhattanDistance(Vertex source, Vertex target) {
         Queue<Vertex> fifo = new ArrayDeque<Vertex>();
         target.setNbr(1);
@@ -129,7 +155,12 @@ public class Labyrinth {
         }
     }
 
-
+	/**
+	 * Calculate the Manhattan distance using the Manhattan algorithm implemented
+	 * in the calculateManhattanDistance algorithm.
+	 * @param source Starting vertex.
+	 * @param target Goal vertex.
+	 */
     public void launchManhattan(Vertex source, Vertex target) {
         for (Vertex vertex : labyrinth.vertexSet())
             vertex.setNbr(0);
@@ -137,36 +168,71 @@ public class Labyrinth {
         calculateManhattanDistance(source, target);
     }
 
+	/**
+	 * Check if there is a wall at the specified direction.
+	 * @param vertex Checked vertex.
+	 * @param dir Checked direction.
+	 * @return A boolean.
+	 */
+
 	public boolean isWall(Vertex vertex,Directions dir) {
 		Vertex dest = getVertexByDir(vertex, dir);
 		Edge edge=labyrinth.getEdge(vertex,dest);
 		return (edge==null);
 	}
 
+	/** Check if the player can go at the specified direction or if
+	 * something is blocking the way.
+	 * @param vertex Starting vertex.
+	 * @param dir Checked direction.
+	 * @return A boolean.
+	 */
 	public boolean isClosed(Vertex vertex,Directions dir){
 		Vertex dest = getVertexByDir(vertex, dir);
 		Edge edge=labyrinth.getEdge(vertex,dest);
 		return (edge==null || (edge.getType()==Edge.Type.CLOSED_DOOR));
 	}
 
+	/** Check if the player can go at the specified direction or if
+	 * something is blocking the way.
+	 * @param vertex Starting vertex.
+	 * @param dir Checked direction.
+	 * @return A boolean.
+	 */
 	public boolean isOpened(Vertex vertex,Directions dir){
 		Vertex dest = getVertexByDir(vertex,dir);
 		Edge edge=labyrinth.getEdge(vertex,dest);
 		return ((edge!=null)&&((edge.getType()!=Edge.Type.CLOSED_DOOR)));
 	}
 
+	/** Check if there is a closed door at the specified direction.
+	 * @param vertex Starting vertex.
+	 * @param dir Checked direction.
+	 * @return A boolean.
+	 */
 	public boolean isClosedDoor(Vertex vertex,Directions dir){
 		Vertex dest = getVertexByDir(vertex, dir);
 		Edge edge=labyrinth.getEdge(vertex,dest);
 		return(edge!=null&&edge.getType()==Edge.Type.CLOSED_DOOR);
 	}
 
+	/** Check if there is a opened door at the specified direction.
+	 * @param vertex Starting vertex.
+	 * @param dir Checked direction.
+	 * @return A boolean.
+	 */
 	public boolean isOpenedDoor(Vertex vertex,Directions dir){
 		Vertex dest = getVertexByDir (vertex, dir);
 		Edge edge=labyrinth.getEdge(vertex,dest);
 		return ((edge!=null)&&((edge.getType()==Edge.Type.OPENED_DOOR)));
 	}
 
+	/** Get the vertex localized at the specified direction from the actual vertex
+	 * and return it.
+	 * @param vertex Actual Vertex
+	 * @param dir Direction from the actual vertex.
+	 * @return The desired vertex.
+	 */
 	private Vertex getVertexByDir(Vertex vertex, Directions dir){
 		Vertex newvertex = null;
 		switch (dir){
@@ -187,12 +253,17 @@ public class Labyrinth {
 	}
 
 
-
+	/** Get an instance of the object Labyrinth.
+	 * Needed because we are using the Singleton Design Pattern.
+	 * @return An instance of Labyrinth.
+	 */
 	public static Labyrinth getInstance() {
 		return instance;
 	}
 
 	@Override
+	/** Write a String version of the content of the labyrinth.
+	 */
 	public String toString() {
 		return labyrinth.toString();
 	}
@@ -229,18 +300,36 @@ public class Labyrinth {
 		}
 	}
 
+	/**
+	 * Check if two vertices are connected by verifying if the labyrinth
+	 * contains a edge between them.
+	 * @param xs Horizontal localisation of the source vertex.
+	 * @param ys Vertical localisation of the source vertex
+	 * @param xt Horizontal localisation of the target vertex.
+	 * @param yt Vertical localisation of the target vertex.
+	 * @return A boolean.
+	 */
 	public boolean areVerticesConnected(int xs, int ys, int xt, int yt) {
 		return labyrinth.containsEdge(new Vertex(xs, ys), new Vertex(xt, yt));
 	}
 
+	/** Return the set of vertices contained in the labyrinth.
+	 * @return a vertex set.
+	 */
 	public Set<Vertex> getVertices() {
 		return labyrinth.vertexSet();
 	}
 
+	/** Return the set of edges contained in the labyrinth.
+	 * @return a edge set.
+	 */
 	public Set<Edge> getPaths() {
 		return labyrinth.edgeSet();
 	}
 
+	/** Return all the walls contained in the labyrinth.
+	 * @return a vertex set.
+	 */
 	public Set<List<Vertex>> getWalls() {
 		Set<List<Vertex>> walls = new HashSet<List<Vertex>>();
 
@@ -285,20 +374,28 @@ public class Labyrinth {
         }
     }
 
-    private Vertex randomVertex(){
+	/**
+	 * Get a random vertex and returns it.
+	 * @return a randon vertex.
+	 */
+	private Vertex randomVertex(){
         int x = ThreadLocalRandom.current().nextInt(WEST_BORDER, EAST_BORDER+1);
         int y = ThreadLocalRandom.current().nextInt(NORTH_BORDER,SOUTH_BORDER+1);
         return new Vertex(x,y);
     }
 
-    public void setLabyrinthGraph(SimpleGraph<Vertex, Edge> graph) {
+	/**
+	 * A setter used in the JUnit tests. We need it because we're using a Singleton design pattern
+	 * for the Labyrinth.
+	 * @param graph a SimpleGraph to be injected in the Labyrinth.
+	 */
+	public void setLabyrinthGraph(SimpleGraph<Vertex, Edge> graph) {
         this.labyrinth = graph;
     }
 
-    public SimpleGraph<?, ?> getLabyrinthGraph() {
-        return this.labyrinth;
-    }
-
+	/**
+	 * Create and replace a all new instance of a Labyrinth object.
+	 */
     public void reset(){
         instance = new Labyrinth();
     }
