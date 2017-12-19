178bf3d4427b23acd347362dae488c0d67d26b72
mduverneix <mduverneix@wylde.emi.u-bordeaux.fr>
2017-12-19 10:57:05 +0100

    Javadoc

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -1,7 +1,15 @@
 package model;
-
+/**
+ * The Enemy is an element which appears in the labyrinth and tries to kill the player.
+ * @author mduverneix
+ *
+ */
 public class Enemy extends Character {
-
+	/**
+	 * Allocates a new Enemy object with a chosen position.
+	 * @param _x the x coordinate we want for the enemy.
+	 * @param _y the y coordinate we want for the enemy.
+	 */
 	public Enemy(int _x, int _y) {
 		super(_x,_y);
 	}
