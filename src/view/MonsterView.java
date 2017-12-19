2745eaf70635e8915ca40762e5bb1eba685b891b
Teiki Pepin <tepepin@emi.u-bordeaux1.fr>
2017-12-19 13:52:10 +0100

    Ajout Javadoc MonsterView

--- src/view/MonsterView.java
+++ src/view/MonsterView.java
@@ -4,6 +4,18 @@ import java.io.File;
 
 public class MonsterView extends ElementView {
 
+	/** 
+	 * Returns a File object that points to an image file representing
+	 * a monster. The File is meant to be used elsewhere in order
+	 * to generate a proper sprite that can be printed on the screen. <p>
+	 * 
+	 * If the file doesn't exist, an empty File will be returned.
+	 *
+	 * @return 			a File object that contains an image of a monster
+	 * @see 			view.ElementView
+	 * @see 			java.io.File
+	 */	
+	
 	@Override
 	protected File getImageFile() {
 		return new File("assets/bad.png");
