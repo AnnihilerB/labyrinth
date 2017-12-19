d58d38f0f9c60e3a0d829546ab56a335b3d50b6d
Teiki Pepin <tepepin@emi.u-bordeaux1.fr>
2017-12-19 14:44:35 +0100

    Ajout Javadoc ElementView

--- src/view/ElementView.java
+++ src/view/ElementView.java
@@ -6,11 +6,38 @@ import javafx.scene.layout.Pane;
 
 import java.io.File;
 
+/** 
+ * Abstract class representing any element that can be represented
+ * graphically at a single position. Provides methods to print the
+ * element on the screen.
+ *
+ * @author 			Teiki Pepin
+ */	
+
 public abstract class ElementView {
 	private ImageView sprite;
 	private double width, height;
+
+	
+	/** 
+	 * Abstract method that should be properly implemented
+	 * by all inheriting classes. It will be used at 
+	 * initialization to build the graphical representation of
+	 * this element.
+	 * 
+	 * @return			a File pointing to an image file representing the element
+	 * @see 			#ElementView()
+	 */
 	
 	protected abstract File getImageFile();
+
+	
+	/** 
+	 * Constructor for all implementing classes. Uses an image file
+	 * that should be provided by the implementing class.
+	 *
+	 * @see 			#getImageFile()
+	 */
 	
 	protected ElementView() {
 		Image image = new Image(getImageFile().toURI().toString());
@@ -20,56 +47,111 @@ public abstract class ElementView {
 		height = image.getHeight();
 	}	
 
+	
+	/** 
+	 * Adds the sprite of this element to the specified pane at the given
+	 * coordinates.
+	 *
+	 * @param	pane	the JavaFX pane in which to draw this element
+	 * @param	x		the x coordinate in the graph at which to draw this element
+	 * @param	y		the y coordinate in the graph at which to draw this element
+	 * @see 			javafx.scene.layout.Pane
+	 */
+	
     public void drawSprite(Pane pane, int x, int y) {		
     	pane.getChildren().add(this.sprite);
     	updatePosition(x, y);
     }
 
+    
+	/** 
+	 * Moves the sprite of this element to the given coordinates.
+	 *
+	 * @param	x		the x coordinate in the graph at which to move this element
+	 * @param	y		the y coordinate in the graph at which to move this element
+	 */
+	
     public void updatePosition(int x, int y) {
     	sprite.setX(getDisplayPositionFromXCoord(x));
     	sprite.setY(getDisplayPositionFromYCoord(y));
     }
     
+    
+	/** 
+	 * Gives the graphical display coordinate (in number of pixels from origin)
+	 * on the horizontal axis from a vertex's coordinate in the graph.
+	 *
+	 * @param	x		the x coordinate in the graph
+	 * @return			the x coordinate in the display
+	 * @see				LabyrinthView
+	 */
+    
     private double getDisplayPositionFromXCoord(int x) {
     	double displayPosition;
     	
-    	// On décale la position au delà du mur à l'extrémité ouest
+    	// We set the starting location to the west-most wall
     	displayPosition = LabyrinthView.WALL;
     	
-    	// On décale la position du nombre de cellules par rapport à l'extrémité ouest
+    	// We move the location eastwards by the number of cells needed
     	displayPosition += x * (LabyrinthView.WALL + LabyrinthView.CELL);
     	
-    	// On multiplie ce déplacement par le scaling d'affichage
+    	// We scale the location by the display scaling factor
     	displayPosition *= LabyrinthView.SPAN;
     	
-    	// On centre l'affichage dans sa cellule courante
+    	// We center the location within the current cell depending on this object's width
     	displayPosition += (LabyrinthView.SPAN * LabyrinthView.CELL - width)/2;
     	
     	return displayPosition;
     }
     
+    
+	/** 
+	 * Gives the graphical display coordinate (in number of pixels from origin)
+	 * on the vertical axis from a vertex's coordinate in the graph.
+	 *
+	 * @param	y		the y coordinate in the graph
+	 * @return			the y coordinate in the display
+	 * @see				LabyrinthView
+	 */
+    
     private double getDisplayPositionFromYCoord(int y) {
     	double displayPosition;
-    	
-    	// On décale la position au delà du mur à l'extrémité nord
+
+    	// We set the starting location to the north-most wall
     	displayPosition = LabyrinthView.WALL;
-    	
-    	// On décale la position du nombre de cellules par rapport à l'extrémité nord
+
+    	// We move the location southwards by the number of cells needed
     	displayPosition += y * (LabyrinthView.WALL + LabyrinthView.CELL);
-    	
-    	// On multiplie ce déplacement par le scaling d'affichage
+
+    	// We scale the location by the display scaling factor
     	displayPosition *= LabyrinthView.SPAN;
-    	
-    	// On centre l'affichage dans sa cellule courante
+
+    	// We center the location within the current cell depending on this object's width
     	displayPosition += (LabyrinthView.SPAN * LabyrinthView.CELL - height)/2;
     	
     	return displayPosition;
     }
+    
 
+	/** 
+	 * Getter that returns an ImageView corresponding to the graphical representation 
+	 * of this element.
+	 *
+	 * @return			the ImageView representing the sprite of this element
+	 */
+    
 	protected ImageView getSprite() {
 		return sprite;
 	}
 
+	
+	/** 
+	 * This method updates the stored graphical representation of this
+	 * object by reading from a file stored on disk.
+	 *
+	 * @param	newSpritePath	the File from which the sprite can be read
+	 */
+    
 	protected void updateSprite(File newSpritePath) {
     	sprite.setImage(new Image(newSpritePath.toURI().toString()));
     }
