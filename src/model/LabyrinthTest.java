5902205b42191b630aa0d9fa8ead8f458aacd9e2
pierrinator <plorson3@gmail.com>
2017-12-19 11:38:19 +0100

    Manhattan réparé, passage en Edge, Tests fonctionnels.

--- src/model/LabyrinthTest.java
+++ src/model/LabyrinthTest.java
@@ -1,13 +1,16 @@
 package model;
 
-import junit.framework.TestCase;
+
+import org.jgrapht.graph.DefaultEdge;
+import org.junit.Assert;
+import org.junit.Test;
 import static model.Labyrinth.Directions.*;
 
 import org.jgrapht.graph.SimpleGraph;
 
-public class LabyrinthTest extends TestCase {
-	
-	static SimpleGraph<Vertex, Edge> labyrinth;	
+public class LabyrinthTest {
+    static SimpleGraph<Vertex,Edge> graph;
+	static Labyrinth labyrinth;
 	static Vertex vertex00;
 	static Vertex vertex01;
 	static Vertex vertex10;
@@ -15,82 +18,55 @@ public class LabyrinthTest extends TestCase {
 
     @org.junit.BeforeClass
     public static void generateLabyrinth() {
-    	labyrinth = new SimpleGraph(Edge.class);
+        graph = new SimpleGraph(Edge.class);
+    	labyrinth = Labyrinth.getInstance();
     	
     	vertex00 = new Vertex(0,0);
     	vertex01 = new Vertex(0,1);
     	vertex10 = new Vertex(1,0);
     	vertex11 = new Vertex(1,1);
     	
-		labyrinth.addVertex(vertex00);
-		labyrinth.addVertex(vertex01);
-		labyrinth.addVertex(vertex10);
-		labyrinth.addVertex(vertex11);
-		
-		labyrinth.addEdge(vertex00, vertex01);
-		labyrinth.addEdge(vertex01, vertex11);
-		labyrinth.addEdge(vertex11, vertex10);
-		System.out.println("test");
-    }
-	
+		graph.addVertex(vertex00);
+        graph.addVertex(vertex01);
+        graph.addVertex(vertex10);
+        graph.addVertex(vertex11);
+
+        graph.addEdge(vertex00, vertex01);
+        graph.addEdge(vertex01, vertex11);
+        graph.addEdge(vertex11, vertex10);
+
+        labyrinth.setLabyrinthGraph(graph);
+		}
 
     @org.junit.Test
     public void testIsClosed() {
-        Labyrinth lab = Labyrinth.getInstance();
-        assertNotNull(vertex00);
-        
-        //assertTrue(lab.isClosed(vertex00, SOUTH));
-        //lab.addVertex
+        Assert.assertTrue(labyrinth.isClosed(vertex00, EAST));
 
     }
 
     @org.junit.Test
     public void testIsOpened() {
-        Labyrinth lab = Labyrinth.getInstance();
-        Vertex v1 = new Vertex(0,0);
-        assertTrue(lab.isOpened(v1, EAST));
+        Assert.assertTrue(labyrinth.isOpened(vertex00, SOUTH));
     }
 
-    @org.junit.Test
-    public void testIsClosedDoor() {
-        Labyrinth lab = Labyrinth.getInstance();
-        Vertex v1 = new Vertex(0,0);
-        assertTrue(lab.isClosedDoor(v1, EAST));
-    }
 
-    @org.junit.Test
-    public void testIsOpenedDoor() {
-        Labyrinth lab = Labyrinth.getInstance();
-        Vertex v1 = new Vertex(0,0);
-        assertTrue(lab.isOpenedDoor(v1, EAST));
-    }
 
     @org.junit.Test
     public void testIsWall() {
-        Labyrinth lab = Labyrinth.getInstance();
-        Vertex v1 = new Vertex(0,0);
-        assertTrue(lab.isWall(v1, SOUTH));
+        Assert.assertTrue(labyrinth.isWall(vertex00, EAST));
     }
 
     @org.junit.Test
     public void testAreVerticesConnected() {
-        Labyrinth lab = Labyrinth.getInstance();
-        assertTrue(lab.areVerticesConnected(1,0,1,1));
+        Assert.assertTrue(labyrinth.areVerticesConnected(1,0,1,1));
     }
     
     @org.junit.Test
     public void testGetInstance() {
+
         Labyrinth lab = Labyrinth.getInstance();
-        assertNotNull(lab);
-        assertTrue(lab instanceof Labyrinth);
-    }
-    
-    @org.junit.Test
-    public void testGetterSetter() {
-        Labyrinth lab = Labyrinth.getInstance();
-        lab.setLabyrinthGraph(labyrinth);
-        SimpleGraph<?, ?> returnLabyrinth = lab.getLabyrinthGraph();
-        
-        assertEquals(labyrinth, returnLabyrinth);
+        Assert.assertNotNull(lab);
+        System.out.println("test");
+        Assert.assertTrue(lab instanceof Labyrinth);
     }
 }
