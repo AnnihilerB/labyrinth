1f117ca7481191ec498fa19f9722c26fe4993b67
pierrinator <plorson3@gmail.com>
2017-12-21 09:10:49 +0100

    Javadoc du test + correction de testisOpened suite au changement dans getvertexbydir.

--- src/test/LabyrinthTest.java
+++ src/test/LabyrinthTest.java
@@ -11,6 +11,8 @@ import org.jgrapht.graph.SimpleGraph;
 
 /**
  * JUnit test class for the Labyrinth class.
+ * We need attributes here because we will have to generate a small Labyrinth
+ * so we can verify if the tests return expected answers.
  * 
  * @author Pierre Lorson
  */
@@ -21,7 +23,12 @@ public class LabyrinthTest {
 	static Vertex vertex00;
 	static Vertex vertex01;
 	static Vertex vertex10;
-	static Vertex vertex11; 
+	static Vertex vertex11;
+
+    /**
+     * Generate a small, known Labyrinth before all tests so we can guess
+     * what results we should have.
+     */
 
     @org.junit.BeforeClass
     public static void generateLabyrinth() {
@@ -45,28 +52,47 @@ public class LabyrinthTest {
         labyrinth.setLabyrinthGraph(graph);
 	}
 
+    /**
+     * Checks if isClosed is working correctly :
+     * the assertion should be true.
+     */
     @org.junit.Test
     public void testIsClosed() {
         Assert.assertTrue(labyrinth.isClosed(vertex00, EAST));
 
     }
 
+    /**
+     * Checks if isOpened is working correctly :
+     * the assertion should be true.
+     */
     @org.junit.Test
     public void testIsOpened() {
-        Assert.assertTrue(labyrinth.isOpened(vertex00, SOUTH));
+        Assert.assertTrue(labyrinth.isOpened(vertex00, NORTH));
     }
 
-
+    /**
+     * Checks if isWall is working correctly :
+     * the assertion should be true.
+     */
     @org.junit.Test
     public void testIsWall() {
         Assert.assertTrue(labyrinth.isWall(vertex00, EAST));
     }
 
+    /**
+     * Checks if areVerticesConnected is working correctly :
+     * The assertion should be true
+     */
     @org.junit.Test
     public void testAreVerticesConnected() {
         Assert.assertTrue(labyrinth.areVerticesConnected(1,0,1,1));
     }
-    
+
+    /**
+     * Checks if the Singleton is working correctly :
+     * The two assertions should return true.
+     */
     @org.junit.Test
     public void testGetInstance() {
 
